b6519de679af8bcf75e28c8e41df0fb943222a35
Hani <haniebrahimi69@yahoo.com>
2020-07-23 16:21:53 +0430

    top product controller

---
+++ api/src/main/java/com/tasnim/trade/eshop/api/TopProductService.java
@@ -0,0 +1,7 @@
+package com.tasnim.trade.eshop.api;
+
+import com.tasnim.trade.eshop.dto.TopProduct;
+
+public interface TopProductService {
+    public TopProduct save(TopProduct topProduct);
+}
---
+++ web-service/src/main/java/com/tasnim/trade/eshop/web/service/TopProductController.java
@@ -0,0 +1,26 @@
+package com.tasnim.trade.eshop.web.service;
+
+import com.tasnim.trade.eshop.api.TopProductService;
+import com.tasnim.trade.eshop.dto.TopProduct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class TopProductController {
+
+    @Autowired
+   TopProductService topProductService;
+
+
+    @PostMapping("/api/v1/top-products")
+    public ResponseEntity<TopProduct> save(@RequestBody TopProduct topProduct)
+    {
+
+
+        return ResponseEntity.ok(topProductService.save(topProduct));
+    }
+}
